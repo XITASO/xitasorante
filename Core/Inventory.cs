9442729d1666b3dd2169e2c08319f4e7324e55ba
Alexander Rampp <alexander.rampp@xitaso.com>
2023-04-15 09:19:02 +0200

    Add inventory

---
+++ Core/Inventory.cs
@@ -0,0 +1,18 @@
+﻿namespace Core;
+
+public class Inventory
+{
+    private readonly Dictionary<string, Ingredient> ingredients = new Dictionary<string, Ingredient>();
+
+    public IEnumerable<Ingredient> Ingredients => ingredients.Values;
+
+    public void RegisterIngredient(Ingredient ingredient)
+    {
+        if (ingredients.ContainsKey(ingredient.Name))
+        {
+            throw new InvalidOperationException($"Ingredient with name '{ingredient.Name}' already exists");
+        }
+        ingredients.Add(ingredient.Name, ingredient);
+    }
+
+}
---
+++ Test/Core/InventoryTests.cs
@@ -0,0 +1,29 @@
+﻿using Core;
+
+namespace Test.Core;
+
+public class InventoryTests
+{
+    [Fact]
+    public void Ingredients_can_be_registered()
+    {
+        var inventory = new Inventory();
+        var ingredient = new IngredientBuilder().Build();
+        inventory.RegisterIngredient(ingredient);
+
+        inventory.Ingredients.Should().BeEquivalentTo(new[] { ingredient });
+    }
+    
+    [Fact]
+    public void Throws_on_ingredient_with_same_name()
+    {
+        var inventory = new Inventory();
+        var ingredient1 = new IngredientBuilder().WithName("Tomato").Build();
+        var ingredient2 = new IngredientBuilder().WithName("Tomato").Build();
+        inventory.RegisterIngredient(ingredient1);
+        var registerSameIngredient = () => inventory.RegisterIngredient(ingredient2);
+
+        registerSameIngredient.Should().Throw<InvalidOperationException>()
+            .And.Message.Should().Contain(ingredient2.Name);
+    }
+}

745a75225e60eccb42b82e96bf30ead7a61ae387
Alexander Rampp <alexander.rampp@xitaso.com>
2023-08-31 11:19:53 +0200

    Fix authentication

--- API/Program.cs
+++ API/Program.cs
@@ -4,12 +4,14 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
 using NewTon.API;
 
+const string corsOrigin = "Access-Control-Allow-Origin";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -18,17 +20,33 @@ builder.Services
     .AddXitasoRanteInfrastructure()
     .AddXitasoRanteCoreDomain();
 
+
+builder.Services.AddCors((options =>
+{
+    options.AddPolicy(name: corsOrigin,
+        policy =>
+        {
+            policy.WithOrigins("*")
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+}));
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.ConfigureSwaggerUiAuthentication(app.Configuration));
 }
 
 app.UseHttpsRedirection();
 
+app.UseCors(corsOrigin);
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();

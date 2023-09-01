de910ea3e98e323d4cb77bfbc982c0d4827b676a
Alexander Rampp <alexander.rampp@xitaso.com>
2023-09-01 20:14:11 +0200

    Add examples for Authorization tests

---
+++ Test.Integration/Recipes/RecipesAuthorizationTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using Test.Integration.Setup;
+
+namespace Test.Integration.Recipes;
+
+public class RecipesAuthorizationTests: ApiTestBase
+{
+    public RecipesAuthorizationTests(ApiTestFixture fixture) : base(fixture)
+    {
+    }
+
+    [Fact]
+    public async Task GetRecipes_ReturnsUnauthorized_OnAnonymousAccess()
+    {
+        var result = await Client.GetAsync("/Recipes");
+        result.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+    
+    [Fact]
+    public async Task GetRecipes_ReturnsForBidden_OnWrongClaim()
+    {
+        var accessToken = new AccessTokenBuilder().WithScopes("Arbitrary.Scope").Build();
+        var result = await Client
+            .WithAuthenticatedUser(accessToken)
+            .GetAsync("/Recipes",
+                // This option is necessary, otherwise you'll get a HttpRequestException on forbidden
+                HttpCompletionOption.ResponseHeadersRead);
+        
+        result.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+    
+    [Fact]
+    public async Task GetRecipes_ReturnsOK_OnAuthorizedAccess()
+    {
+        var accessToken = new AccessTokenBuilder().WithScopes("Recipes.Read").Build();
+        var result = await Client
+            .WithAuthenticatedUser(accessToken)
+            .GetAsync("/Recipes");
+        
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}

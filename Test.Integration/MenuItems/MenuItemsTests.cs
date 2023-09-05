475f478414a145d9026fb7b3ca5f538022317b61
Alexander Rampp <alexander.rampp@xitaso.com>
2023-09-05 17:59:18 +0200

    Add simple test for menu items

---
+++ Test.Integration/MenuItems/MenuItemsTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+using API.Controllers;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Test.Integration.MenuItems;
+
+public class MenuItemsTests
+{
+    [Fact]
+    public async Task GetMenuItems_ReturnsMenuItems()
+    {
+        var factory = new WebApplicationFactory<IngredientsController>();
+        var client = factory.CreateClient();
+
+        var result = await client.GetFromJsonAsync<IEnumerable<DishDto>>("/MenuItems");
+
+        result.Should().NotBeEmpty();
+    }
+}
